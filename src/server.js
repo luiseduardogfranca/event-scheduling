90e4a62194b62332031ad58b032f674cee898016
Luís Eduardo <luiseduardo@Luiss-MacBook-Pro.local>
2020-08-28 10:55:49 -0300

    fix bind port to heroku

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ const app = require("./app")();
 
 (async () => {
   // call appLoader
-  app.listen(apiConfig.API_PORT, () => {
+  app.listen(process.env.PORT || apiConfig.API_PORT, () => {
     logLoader.info(
-      `API is running at https://localhost:${apiConfig.API_PORT} in ${app.get(
-        "env"
-      )} mode`
+      `API is running at https://localhost:${process.env.PORT ||
+        apiConfig.API_PORT} in ${app.get("env")} mode`
     );
   });
 })();
